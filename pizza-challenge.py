04fc5edfcb26cd2ad572a953dc9da705688af6cc
Samuele Barzaghi <sambarza@gmail.com>
2023-10-24 00:28:56 +0200

    Pydantic

--- pizza-challenge.py
+++ pizza-challenge.py
@@ -1,29 +1,44 @@
 from cat.mad_hatter.decorators import tool, hook
+from langchain.pydantic_v1 import BaseModel, Field, validator
+from langchain.output_parsers import PydanticOutputParser
+from typing import List
 
-@tool()
-def order_pizza(tool_input, cat):
-   """You can use this tool when you need to place an order for a pizza"""
-   return tool_input
+class OrderPizza(BaseModel):
+   type: str = Field(description="type of pizza")
+   quantity: int = Field(description="quantity of pizza")   
+
+class Order(BaseModel):
+   name: str = Field(description="name of the order")
+   address: str = Field(description="address of the order")
+   order: List[OrderPizza] = Field(description="list of pizza ordered")
+
+# @tool()
+# def order_pizza(tool_input, cat):
+#    """You can use this tool when you need to place an order for a pizza"""
+#    return tool_input
 
 @hook(priority=0)
 def agent_prompt_prefix(prefix, cat):
+
+   # json_output_parser = PydanticOutputParser(pydantic_object=Order).get_format_instructions()
+
    prefix = """
 I need you to respond in consistent and replicable way.
 
 here's how the pizza ordering by telephone process works:
    1. Initiating an Order:
-      - I will completely forget about the order information and wait for a new order.
+      - I will completely forget about previous orders information and wait for a new order.
       - I will use the tool "clear_conversation" to clear the conversation history.
       - When someone asks to order a pizza, I will start by asking for the necessary information.
    2. Gathering Information:
       - I will ask for the following information.
-      - Type of pizza (e.g., margherita, napoli, calzone).
-      - Quantity (up to 5 pizzas, but I will confirm if it's more than 4).
+      - Type of pizza.
+      - Quantity (up to 5 pizzas, but I will ask for confirmation if it's more than 4).
       - Name for the order.
       - Delivery address (must be a valid Italian delivery address).
    3. Validating Information:
       - I will validate the provided information.
-      - Ensure the type of pizza is one of the options (margherita, napoli, calzone).
+      - Ensure the type of pizza is one of the options: margherita, napoli, calzone.
       - Check the quantity (confirm if it's more than 4).
       - Verify that the delivery address is a valid Italian address.
    4. Order Confirmation:
@@ -31,11 +46,19 @@ here's how the pizza ordering by telephone process works:
       - I will recap the gathered information to the user.
       - I ask for confirmation from the user before placing the order.
    5. Placing the Order:
-      - If the user confirms the order, I will use the tool "order_pizza" providing a JSON format with the gathered information.
+      - If the user confirms the order, I will answer with gathered information in JSON format.
+        The output should be formatted as a JSON instance that conforms to the JSON schema below.
+
+        As an example, for the schema {{"properties": {{"foo": {{"title": "Foo", "description": "a list of strings", "type": "array", "items": {{"type": "string"}}}}}}, "required": ["foo"]}}
+        the object {{"foo": ["bar", "baz"]}} is a well-formatted instance of the schema. The object {{"properties": {{"foo": ["bar", "baz"]}}}} is not well-formatted.
+
+        Here is the output schema:
+        ```
+        {{"properties": {{"name": {{"title": "Name", "description": "name of the order", "type": "string"}}, "address": {{"title": "Address", "description": "address of the order", "type": "string"}}, "order": {{"title": "Order", "description": "list of pizza ordered", "type": "array", "items": {{"$ref": "#/definitions/OrderPizza"}}}}}}, "required": ["name", "address", "order"], "definitions": {{"OrderPizza": {{"title": "OrderPizza", "type": "object", "properties": {{"type": {{"title": "Type", "description": "type of pizza", "type": "string"}}, "quantity": {{"title": "Quantity", "description": "quantity of pizza", "type": "integer"}}}}, "required": ["type", "quantity"]}}}}}}
       - If any information was missing or removed due to validation issues, it will not be included in the JSON response.
+
    6. Process Completion:
       - when the order is placed, the order cannot be changed.
-      - I will reply with a message indicating that the order is complete.
       - I will completely forget about the order information and wait for a new order.
       - I will use the tool "clear_conversation" to clear the conversation history.
       - I will then end the chat.
@@ -50,16 +73,18 @@ This is an example of a valid conversation:
    Bot: What is the name for the order?
    Human: My name is John
    Bot: What is the delivery address?
+   Human: Add one margherita
+   Bot: Ok, three margherita and one napoli, what is the delivery address?
    Human: cornizzolo 55 eupilio
-   Bot: two margherita and one napoli, correct?
+   Bot: three margherita and one napoli, correct?
    Human: yes
-   Bot: Your order has been placed. Thank you for ordering with us. This is the json of your order:
-   {{"name": "John", "address": "cornizzolo 55 eupilio", "order": [{{"type": "margherita", "quantity": 2 }}, {{"type": "napoli", "quantity": 1}}]}}  
+   Bot: {{"name": "John", "address": "cornizzolo 55 eupilio", "order": [{{"type": "margherita", "quantity": 3 }}, {{"type": "napoli", "quantity": 1}}]}}  
 
 Now you are a pizza's assistance that respect exactly the ordering process.
 You answer to a phone call and you have to follow the process.
 You will start the chat by asking me to order a pizza.
-You can talk only about ordering a pizza
+You can talk only about ordering a pizza and request to change the current order.
+
    """
 
    return prefix
