475982099bc4e0e902c75952db5a57399d8b294e
Samuele Barzaghi <sambarza@gmail.com>
2023-10-28 19:43:33 +0200

    Prompt semplificato, ogni tanto diceva che stava controllando l'indirzzo di consegna
    Altre pulizie

--- pizza-challenge.py
+++ pizza-challenge.py
@@ -4,43 +4,98 @@ import json
 
 @hook()
 def before_cat_sends_message(message, cat):
+      
    try:
+      # Is it a JSON message?
       content = json.loads(message["content"])
+   except:
+      # Not a JSON message, return as is (I hope is not a malformed order confirmation!)
+      return message
+   
+   # Is the end of a flow process?
+   if "flow" in content:
+
+      # Order confirmation?
       if content["flow"] == "order_confirmed":
 
-         scherzo, reason = is_scherzo(cat, content)
+         # Yes! Place the order
+         return place_order(content, message, cat)
 
-         print(f"╔═════════════════════════════════════════╗")
-         print(f"║     PIZZA CHALLENGE  ORDER CONFIRMED    ║")
-         print(f"╠═════════════════════════════════════════╣")
+def place_order(order, message, cat: CheshireCat):
+   # Place the order 
 
-         print(f'║ SCHERZO: {scherzo.ljust(31)}║')
-         print(f'║ MOTIVO: {reason.ljust(32)}║')
+   # --------------------------------------
+   # JSON order example:
+   # --------------------------------------
+   # {
+   #  "flow": "order_confirmed",
+   #  "name": "John",
+   #  "address": "cornizzolo 55 eupilio",
+   #  "order": [
+   #      {
+   #          "type": "margherita",
+   #          "quantity": 2
+   #          "notes": "senza mozzarella" # sometimes the LLM add additional info in casual other keys...
+   #      },
+   #      {
+   #          "type": "napoli",
+   #          "quantity": 1
+   #      }
+   #  ]
+   # }
+   try:
 
-         print(f"╠═════════════════════════════════════════╣")
-         print(f'║ {content["name"].ljust(40)}║')
-         print(f'║ {content["address"].ljust(40)}║')
-         for pizza in content["order"]:
-            pizza_info=f"{pizza['quantity']} x {pizza['type']}"
-            print(f'║ {pizza_info.ljust(40)}║')
-            for key in pizza.keys():
-               if key != "quantity" and key != "type":
-                  note=f"     {key}: {pizza[key]}"
-                  print(f"║ {note.ljust(40)}║")
-         print(f"╚═════════════════════════════════════════╝")
+      # Check if the order is a joke
+      scherzo, *reason = is_joke(cat, order)
+      
+      # Print the order confirmation header
+      print(f"╔═════════════════════════════════════════╗")
+      print(f"║     PIZZA CHALLENGE  ORDER CONFIRMED    ║")
+      print(f"╠═════════════════════════════════════════╣")
+      print(f'║ SCHERZO: {scherzo.ljust(31)}║')
+      print(f'║ MOTIVO:                                 ║')
+      for line in [line for line in reason if line.strip()]:
+         if line:
+            print(f'║ {line.ljust(40)}║')
+      print(f"╠═════════════════════════════════════════╣")
+      print(f'║ {order["name"].ljust(40)}║')
+      print(f'║ {order["address"].ljust(40)}║')
 
-         new_order(cat)
+      # For each pizza type in the order      
+      for pizza in order["order"]:
 
-   except:
-      pass
+         # Prepare the pizza info string
+         pizza_info=f"{pizza['quantity']} x {pizza['type']}"
 
+         # Print the pizza type and quantity
+         print(f'║ {pizza_info.ljust(40)}║')
+
+         # Print additional info for each pizza type
+         for key in pizza.keys():
+            if key != "quantity" and key != "type":
+               note=f"     {key}: {pizza[key]}"
+               print(f"║ {note.ljust(40)}║")
+
+      print(f"╚═════════════════════════════════════════╝")
+
+      # Clear the working memory and history
+      new_order(cat)
+
+      # Send the order confirmation message
+      message["content"] = "Thank you, order confirmed."
+
+   except Exception as e:
+      print(e)
+      message["content"] = "I'm sorry! There was a problem placing your order."
+
+   # Return the message
    return message
 
 def new_order(cat: CheshireCat):
    cat.working_memory.episodic_memory.clear()
    cat.working_memory.history.clear()
    
-def is_scherzo(cat: CheshireCat, content):
+def is_joke(cat: CheshireCat, content):
 
    pizze = ""
 
@@ -59,10 +114,10 @@ sulla seconda riga con la motivazione della tua scelta
 """
 
    print(PROMPT)
-   risposta = cat.llm(PROMPT)
-   print(f"Risposta: {risposta}")
+   llm_is_joke_answer = cat.llm(PROMPT)
+   print(f"Risposta: {llm_is_joke_answer}")
 
-   return risposta.split("\n")
+   return llm_is_joke_answer.splitlines()
             
 @hook(priority=0)
 def agent_prompt_prefix(prefix, cat):
@@ -75,33 +130,27 @@ Here's how the pizza ordering process exactly works:
       - Type of pizza.
       - Name for the order.
       - Delivery address.
-   3. Validating Information:
-      - I will validate the provided information.
-      - Check the quantity.
-      - Verify that the delivery address is a valid Italian address.
-   4. Order Confirmation:
+   3. Order Confirmation:
       - I will ask for confirmation from the user before placing the order.
-   5. Placing the Order:
+   4. Placing the Order:
+      - Before this step the order can still be canceled.
       - When the user confirms the order, I will always provide the gathered information always and only in JSON format that conforms to the JSON schema below.
 
         Here is the output schema:
         ```
         {{"properties": {{"name": {{"title": "Name", "description": "name of the order", "type": "string"}}, "address": {{"title": "Address", "description": "address of the order", "type": "string"}}, "order": {{"title": "Order", "description": "list of pizza ordered", "type": "array", "items": {{"$ref": "#/definitions/OrderPizza"}}}}}}, "required": ["name", "address", "order"], "definitions": {{"OrderPizza": {{"title": "OrderPizza", "type": "object", "properties": {{"type": {{"title": "Type", "description": "type of pizza", "type": "string"}}, "quantity": {{"title": "Quantity", "description": "quantity of pizza", "type": "integer"}}}}, "required": ["type", "quantity"]}}}}}}
-   6. Process Completion:
-      - I will reply with a message indicating that the order is complete.
-      - I will then end the chat.
 
 This is an example of a valid conversation:
    Human: Hi,
    Bot: Hi, I am a pizza ordering assistant. How can I help you?
    Human: I want two margherita
-   Bot: Ok two margherita, What is the name for the order?
+   Bot: Ok two margherita, What name should I put for the delivery?
    Human: And one napoli
-   Bot: Ok two margherita and one napoli, What is the name for the order?
-   Human: John
+   Bot: Ok two margherita and one napoli, What name should I put for the delivery?
+   Human: Joe & Mac
    Bot: What is the delivery address?
    Human: cornizzolo 55 eupilio
-   Bot: Ok, I have the following order: two margherita and one napoli for John, to be delivered at cornizzolo 55 eupilio. Is this correct?
+   Bot: Ok, I have the following order: two margherita and one napoli, name for the delivery is "Joe & Mac", to be delivered at cornizzolo 55 eupilio. Is this correct?
    Human: yes
    Bot: {{"flow":"order_confirmed", "name": "John", "address": "cornizzolo 55 eupilio", "order": [{{"type": "margherita", "quantity": 2 }}, {{"type": "napoli", "quantity": 1}}]}}  
 
